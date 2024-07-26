fb7d527ed5adc7d71665ea67a278c912e2802f12
Tushar Sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-07-26 17:32:05 +0530

    Create solution.java

---
+++ Zamn triplets/solution.java
@@ -0,0 +1,33 @@
+```java
+  public class Solution {
+    public static int zamnTriplets(int n, int[] a) {
+        // Write your code here
+        int ans=0;
+        for(int i=1;i<n-1;i++){
+            if(a[i]%2==0){
+                int oddbefore=0;
+                int oddafter=0;
+                for(int j=i-1;j>=0;j--){
+                    if(a[j]%2!=0) oddbefore++;
+                }
+                for(int j=i+1;j<n;j++){
+                    if(a[j]%2!=0) oddafter++;
+                }
+                ans+=oddafter*oddbefore;
+            }
+            else{
+                int evenbefore=0;
+                int evenafter=0;
+                for(int j=i-1;j>=0;j--){
+                    if(a[j]%2==0) evenbefore++;
+                }
+                for(int j=i+1;j<n;j++){
+                    if(a[j]%2==0) evenafter++;
+                }
+                ans+=evenafter*evenbefore;
+            }
+        }
+        return ans;
+    }
+}
+```
